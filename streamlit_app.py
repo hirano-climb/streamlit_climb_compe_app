a525b3f2365796a097cbc2d8c58e7a1594771c50
koto-papa <164696257+koto-papa@users.noreply.github.com>
2024-06-11 21:53:05 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,24 +7,25 @@ import pandas as pd
 import openpyxl as op
 from io import BytesIO
 
+def main():
+    image = Image.open('./data/photo-165790233.jpg')
+    st.set_page_config(
+        page_title="climb compe result", 
+        page_icon=image, 
+        layout="centered", 
+        initial_sidebar_state="auto"
+        )
 
-image = Image.open('./data/photo-165790233.jpg')
-st.set_page_config(
-    page_title="climb compe result", 
-    page_icon=image, 
-    layout="centered", 
-    initial_sidebar_state="auto"
-    )
+    page_names = {
+        "入力": input.input, 
+        "管理": admin.admin
+        }
+    pages = st.sidebar.selectbox(
+        "メニュー", 
+        page_names.keys()
+        )
+    page_names[pages]()
 
-page_names = {
-    "入力": input.input, 
-    "管理": admin.admin
-    }
-pages = st.sidebar.selectbox(
-    "メニュー", 
-    page_names.keys()
-    )
-page_names[pages]()
-
-
+if __name__ == '__main__':
+	main()
 
